bdb582622d402c70046697ed5ab5c646fc23221a
Gawain B. II <bracy.gawain2014@gmail.com>
2021-01-01 18:03:17 -0500

    updated nav

---
+++ precache-manifest.0673110f01314a34c696eda4c2ac6e68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ebc5755873b46101017",
+    "url": "/portfolio/static/css/main.9e4ab101.chunk.css"
+  },
+  {
+    "revision": "6ebc5755873b46101017",
+    "url": "/portfolio/static/js/main.6ebc5755.chunk.js"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "d6ce300691c037d84277519aa29bc058",
+    "url": "/portfolio/index.html"
+  }
+];
